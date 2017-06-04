6ec95b15d7da2f427ad615ec1ef0fa7cdc044c9a
hongqingWang <272338444@qq.com>
2017-06-04 17:12:02 +0800

    代码完成

--- HQChannelListView/AppDelegate.swift
+++ HQChannelListView/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = ViewController()
+        let nav = UINavigationController(rootViewController: vc)
+        self.window?.rootViewController = nav
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
--- HQChannelListView/ViewController.swift
+++ HQChannelListView/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var myChannels = ["推荐", "热点", "北京", "视频",
+                      "社会", "娱乐", "问答", "汽车",
+                      "财经", "军事", "体育", "段子",
+                      "美女", "时尚", "国际", "趣图",
+                      "健康", "特卖", "房产", "养生",
+                      "历史", "育儿", "小说", "教育",
+                      "搞笑"]
+    var moreChannels = ["科技", "直播", "数码", "美食",
+                        "电影", "手机", "旅游", "股票",
+                        "科学", "动漫", "故事", "收藏",
+                        "精选", "语录", "星座", "美图",
+                        "政务", "辟谣", "火山直播", "中国新唱将",
+                        "彩票", "快乐男生", "正能量"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.title = "王红庆"
+        view.backgroundColor = UIColor.white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(popToChannelListViewController))
     }
+    
+    func popToChannelListViewController() -> () {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let channelVC = HQChannelListViewController(myChannel: myChannels, moreChannel: moreChannels)
+        channelVC.selectCallBack = { (myChannel, moreChannel, selectIndex) -> () in
+            self.navigationItem.title = myChannel[selectIndex]
+            self.myChannels = myChannel
+            self.moreChannels = moreChannel
+        }
+        navigationController?.pushViewController(channelVC, animated: true)
     }
-
-
 }
 
